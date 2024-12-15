8beff164e87279fd32461060412f77f896d2d7fa
pether-pg <pether.pg@gmail.com>
2024-12-15 01:16:11 +0100

    WoodenSetFurnitures - Nov24 QoL fixes
    WoodenSetFurnitures - Nov24 QoL fixes

--- WoodenSetFurniture/Properties/AssemblyInfo.cs
+++ WoodenSetFurniture/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2024.6.20.1654")]
-[assembly: AssemblyFileVersion("2024.6.20.1654")]
+[assembly: AssemblyVersion("2024.12.15.0007")]
+[assembly: AssemblyFileVersion("2024.12.15.0007")]
